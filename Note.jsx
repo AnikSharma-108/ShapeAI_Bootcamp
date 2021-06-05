70923811863f7b3e76d60cf564737e6bd96886c0
AnikSharma-108 <85402735+AnikSharma-108@users.noreply.github.com>
2021-06-05 20:24:27 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -3,8 +3,8 @@ function Note() {
   return (
     <div className="note">
       <h1>BASIC WEB DEVELOPMENT</h1>
-      <p>This was very nice bootcamp by Shape AI and its instructor Mr.Shaurya Sinha.</p>
-      <p>It created good and strong basic about JavaScript , React.js and etc.</p>
+      <p>I attended this bootcamp by Shape AI under the instructor, Mr.Shaurya Sinha.</p>
+      <p>It created good understanding about JavaScript , React.js and etc. and also, got to have hands on experience on them.</p>
     </div>
   );
 }
